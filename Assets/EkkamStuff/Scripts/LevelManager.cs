40c4b8b4687df835a9cd5c14f90bfcd459d99aed
Ekkamjeet Singh <ekkam04yt@gmail.com>
2023-10-24 00:10:33 +0400

    Finalized lakshya scene 1

--- Assets/EkkamStuff/Scripts/LevelManager.cs
+++ Assets/EkkamStuff/Scripts/LevelManager.cs
@@ -43,16 +43,26 @@ public class LevelManager : MonoBehaviour
 
         // if only one player isEliminated false is left active, end the round
         int playersEliminated = 0;
+        int teamBlueEliminated = 0;
+        int teamRedEliminated = 0;
         foreach (Player player in players)
         {
             if (player.isEliminated)
             {
                 playersEliminated++;
+                if (player.playerNumber == 1 || player.playerNumber == 2)
+                {
+                    teamRedEliminated++;
+                }
+                else if (player.playerNumber == 3 || player.playerNumber == 4)
+                {
+                    teamBlueEliminated++;
+                }
             }
         }
         if (
             (playersEliminated == players.Length - 1 && GameManager.instance.modeText.text == "Solo" && roundStarted) ||
-            (playersEliminated == players.Length - 2 && GameManager.instance.modeText.text == "Duos" && roundStarted)
+            (playersEliminated == players.Length - 2 && (teamRedEliminated == 2 || teamBlueEliminated == 2) && GameManager.instance.modeText.text == "Duos" && roundStarted)
            )
         {
             roundStarted = false;
--- Assets/LakshyaStuff/Scripts/platformmanager.cs
+++ Assets/LakshyaStuff/Scripts/platformmanager.cs
@@ -1,35 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class platformmanager : MonoBehaviour
 {
-    public GameObject[] platforms; 
+    // public GameObject[] platforms; 
+    public List<GameObject> platforms = new List<GameObject>();
     public GameObject indicatorPrefab; 
     public float destructionInterval = 5.0f; 
-    public float indicatorDuration = 2.0f; 
+    // public float indicatorDuration = 2.0f; 
+    public float timer;
 
     private void Start()
     {
-        StartCoroutine(DestroyRandomPlatformWithIndicator());
+        // StartCoroutine(DestroyRandomPlatformWithIndicator());
     }
 
-    IEnumerator DestroyRandomPlatformWithIndicator()
+    private async void Update()
     {
-        while (true)
+        timer += Time.deltaTime;
+        if (timer >= destructionInterval && platforms.Count > 1 && GameManager.instance.gameStarted)
         {
-            yield return new WaitForSeconds(destructionInterval);
-
-            int randomIndex = Random.Range(0, platforms.Length);
+            timer = 0.0f;
+            int randomIndex = Random.Range(0, platforms.Count);
             GameObject platformToDestroy = platforms[randomIndex];
+            platforms.RemoveAt(randomIndex);
 
-            GameObject indicator = Instantiate(indicatorPrefab, platformToDestroy.transform.position, Quaternion.identity);
-
-            Destroy(indicator, indicatorDuration);
-
-            yield return new WaitForSeconds(indicatorDuration);
+            // jitter the platform
+            for (int i = 0; i < 30; i++)
+            {
+                Vector3 jitter = new Vector3(Random.Range(-0.1f, 0.1f), 0f, Random.Range(-0.1f, 0.1f));
+                platformToDestroy.transform.position += jitter;
+                await Task.Delay(50);
+                platformToDestroy.transform.position -= jitter;
+            }
 
             Destroy(platformToDestroy);
         }
     }
+
+    // IEnumerator DestroyRandomPlatformWithIndicator()
+    // {
+    //     while (true)
+    //     {
+    //         yield return new WaitForSeconds(destructionInterval);
+
+    //         int randomIndex = Random.Range(0, platforms.Length);
+    //         GameObject platformToDestroy = platforms[randomIndex];
+
+    //         // GameObject indicator = Instantiate(indicatorPrefab, platformToDestroy.transform.position, Quaternion.identity);
+
+    //         // Destroy(indicator, indicatorDuration);
+
+    //         // jitter the platform
+    //         for (int i = 0; i < 30; i++)
+    //         {
+    //             Vector3 jitter = new Vector3(Random.Range(-0.1f, 0.1f), 0f, Random.Range(-0.1f, 0.1f));
+    //             platformToDestroy.transform.position += jitter;
+    //             yield return new WaitForSeconds(0.1f);
+    //             platformToDestroy.transform.position -= jitter;
+    //         }
+
+    //         // yield return new WaitForSeconds(indicatorDuration);
+
+    //         Destroy(platformToDestroy);
+    //     }
+    // }
 }
